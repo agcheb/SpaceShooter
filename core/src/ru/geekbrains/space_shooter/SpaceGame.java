157d0e27181f17fd9241cf2f877644415c182b3f
agcheb <agchebykin@gmail.com>
2017-08-16 00:22:21 +0300

    add img.parrots. Change back font into parrots img, move

--- core/src/ru/geekbrains/space_shooter/SpaceGame.java
+++ core/src/ru/geekbrains/space_shooter/SpaceGame.java
@@ -9,19 +9,22 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public class SpaceGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
+	Texture backimg;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
+		backimg = new Texture("parrots.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		batch.draw(backimg,0,0,640,480);
+		batch.draw(img, 0,400,80,80);
 		batch.end();
 	}
 	

bb6b97b4eead70e612da3a37ecf64adf7bc070df
agcheb <agchebykin@gmail.com>
2017-08-15 22:30:14 +0300

    start lib_gdx

---
+++ android/src/ru/geekbrains/space_shooter/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ru.geekbrains.space_shooter;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ru.geekbrains.space_shooter.SpaceGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpaceGame(), config);
+	}
+}
---
+++ desktop/src/ru/geekbrains/space_shooter/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package ru.geekbrains.space_shooter.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import ru.geekbrains.space_shooter.SpaceGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceGame(), config);
+	}
+}

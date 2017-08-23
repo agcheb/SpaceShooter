91a28b69be9392dc00d9d764957e0f0bf69eeb59
agcheb <agchebykin@gmail.com>
2017-08-23 21:15:32 +0300

    del spacegame

--- core/src/ru/geekbrains/space_shooter/StarGame.java
+++ core/src/ru/geekbrains/space_shooter/StarGame.java
@@ -2,6 +2,8 @@ package ru.geekbrains.space_shooter;
 
 import com.badlogic.gdx.Game;
 
+import ru.geekbrains.space_shooter.screens.menu.MenuScreen;
+
 /**
  * Created by agcheb on 23.08.17.
  */
@@ -9,6 +11,6 @@ import com.badlogic.gdx.Game;
 public class StarGame extends Game {
     @Override
     public void create() {
-
+        setScreen(new MenuScreen());
     }
 }
--- desktop/src/ru/geekbrains/space_shooter/desktop/DesktopLauncher.java
+++ desktop/src/ru/geekbrains/space_shooter/desktop/DesktopLauncher.java
@@ -2,11 +2,12 @@ package ru.geekbrains.space_shooter.desktop;
 
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import ru.geekbrains.space_shooter.SpaceGame;
+
+import ru.geekbrains.space_shooter.StarGame;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new SpaceGame(), config);
+		new LwjglApplication(new StarGame(), config);
 	}
 }

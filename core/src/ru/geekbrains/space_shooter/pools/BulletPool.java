a44f9884f53b8988b1a04a525b1c8cca4660a682
agcheb <agchebykin@gmail.com>
2017-09-11 12:32:12 +0300

    add pool

---
+++ core/src/ru/geekbrains/space_shooter/pools/BulletPool.java
@@ -0,0 +1,8 @@
+package ru.geekbrains.space_shooter.pools;
+
+/**
+ * Created by agcheb on 11.09.17.
+ */
+
+public class BulletPool {
+}
---
+++ engine/src/ru/geekuniversity/engine/pool/SpritesPool.java
@@ -0,0 +1,8 @@
+package ru.geekuniversity.engine.pool;
+
+/**
+ * Created by agcheb on 11.09.17.
+ */
+
+public class SpritesPool {
+}
